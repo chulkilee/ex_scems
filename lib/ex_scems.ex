6576cbe030093218c891f26ca9c2efe4f1730158
Chulki Lee <chulki.lee@gmail.com>
2017-12-08 13:28:14 -0800

    initial commit

---
+++ lib/ex_scems.ex
@@ -0,0 +1,18 @@
+defmodule ExSCEMS do
+  @moduledoc """
+  Documentation for ExSCEMS.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExSCEMS.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_scems_test.exs
@@ -0,0 +1,8 @@
+defmodule ExSCEMSTest do
+  use ExUnit.Case
+  doctest ExSCEMS
+
+  test "greets the world" do
+    assert ExSCEMS.hello() == :world
+  end
+end
