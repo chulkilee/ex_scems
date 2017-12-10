bacf311b4cd6c5139bfc03651ab7980a7139f965
Chulki Lee <chulki.lee@gmail.com>
2017-12-09 22:48:51 -0800

    Add login_by_vendor

--- lib/ex_scems.ex
+++ lib/ex_scems.ex
@@ -3,7 +3,9 @@ defmodule ExSCEMS do
   Documentation for ExSCEMS.
   """
 
-  alias ExSCEMS.{Client, Config}
+  import SweetXml
+
+  alias ExSCEMS.{Client, Config, Response}
 
   #
   # Request
@@ -26,4 +28,39 @@ defmodule ExSCEMS do
 
   defp build_url(endpoint, path, []), do: endpoint <> path
   defp build_url(endpoint, path, query), do: endpoint <> path <> "?" <> URI.encode_query(query)
+
+  #
+  # Authentication
+  #
+
+  @doc """
+  [Vendor Login](http://documentation.sentinelcloud.com/wsg/verifyLogin.htm)
+  """
+  @spec login_by_vendor(String.t(), String.t(), String.t()) ::
+          {:ok, Response.t(), String.t()} | {:error, Response.t() | any}
+  def login_by_vendor(endpoint, username, password),
+    do: do_login(endpoint, "/verifyLogin.xml", userName: username, password: password)
+
+  defp do_login(endpoint, path, form) do
+    case Client.request(:post, build_url(endpoint, path, []), {:form, form}, [], []) do
+      {:ok, resp} -> parse_login_response(resp)
+      {:error, error} -> {:error, error}
+    end
+  end
+
+  defp parse_login_response(%Response{body_xml: body_xml} = resp) do
+    case body_xml
+         |> xpath(~x"/emsResponse/sessionId")
+         |> xml_text() do
+      nil -> {:error, resp}
+      val -> {:ok, resp, val}
+    end
+  end
+
+  #
+  # XML
+  #
+
+  defp xml_text(nil), do: nil
+  defp xml_text(xml), do: xpath(xml, ~x"//text()"s)
 end
--- test/ex_scems_test.exs
+++ test/ex_scems_test.exs
@@ -1,4 +1,73 @@
 defmodule ExSCEMSTest do
   use ExUnit.Case, async: true
   doctest ExSCEMS
+
+  alias ExSCEMS.{Config, Response}
+
+  @session_id "FAKE_SESSION_ID"
+
+  setup do
+    bypass = Bypass.open()
+
+    config = %Config{
+      endpoint: "http://127.0.0.1:#{bypass.port}",
+      session_id: @session_id
+    }
+
+    {:ok, bypass: bypass, config: config}
+  end
+
+  #
+  # Authentication
+  #
+
+  test "login_by_vendor - success", %{bypass: bypass} do
+    Bypass.expect_once(bypass, "POST", "/verifyLogin.xml", fn conn ->
+      conn
+      |> assert_request_body(%{"userName" => "foo", "password" => "bar"})
+      |> Plug.Conn.put_resp_header("Content-Type", "application/xml;charset=UTF-8")
+      |> Plug.Conn.resp(200, """
+         <?xml version="1.0" encoding="UTF-8"?>
+         <emsResponse>
+           <sessionId>F53FD7E013F8358C936851EF5D6835CD</sessionId>
+           <stat>ok</stat>
+         </emsResponse>
+         """)
+    end)
+
+    {:ok, %Response{stat: "ok"}, "F53FD7E013F8358C936851EF5D6835CD"} =
+      ExSCEMS.login_by_vendor("http://127.0.0.1:#{bypass.port}", "foo", "bar")
+  end
+
+  test "login_by_vendor - fail", %{bypass: bypass} do
+    Bypass.expect_once(bypass, "POST", "/verifyLogin.xml", fn conn ->
+      conn
+      |> assert_request_body(%{"userName" => "foo", "password" => "bar"})
+      |> Plug.Conn.put_resp_header("Content-Type", "application/xml;charset=UTF-8")
+      |> Plug.Conn.resp(200, """
+         <?xml version="1.0" encoding="UTF-8"?>
+         <emsResponse>
+          <code>215</code>
+          <desc>Incorrect user name or password provided.</desc>
+          <stat>fail</stat>
+         </emsResponse>
+         """)
+    end)
+
+    {:error, %Response{
+      error_code: "215",
+      error_desc: "Incorrect user name or password provided.",
+      stat: "fail"
+    }} = ExSCEMS.login_by_vendor("http://127.0.0.1:#{bypass.port}", "foo", "bar")
+  end
+
+  #
+  # Util
+  #
+
+  defp assert_request_body(conn, expected) do
+    conn2 = Plug.Parsers.call(conn, Plug.Parsers.init(parsers: [:urlencoded]))
+    assert expected == conn2.body_params
+    conn
+  end
 end
